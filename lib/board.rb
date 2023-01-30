e9294109deeb5240e446d5a5c81d22fe3da0ecb9
Ruiyot Abby <ruiyotabby@gmail.com>
2023-01-30 17:13:17 +0300

    Add #add_piece method which adds a player's piece to the board

--- lib/board.rb
+++ lib/board.rb
@@ -1,4 +1,10 @@
 class Board
+  attr_reader :board
+
+  def initialize
+    @board = make_board
+  end
+
   def make_board
     empty_circle = "\u25ef"
     arr = []
@@ -8,6 +14,13 @@ class Board
         j.zero? || j == 7 ? arr[j] << i : arr[j] << empty_circle
       end
     end
-    arr
+    return arr
+  end
+
+  def add_piece(color, column)
+    row = check_piece(column)
+    return puts 'Failed, column already filled' if row.zero?
+
+    @board[row][column] = color
   end
 end
