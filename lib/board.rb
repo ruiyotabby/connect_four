0166090da275982a6d8c6613f1fedd7bd1239741
Ruiyot Abby <ruiyotabby@gmail.com>
2023-02-01 13:21:22 +0300

    Add #check_column method which checks columns in the board for 4 consecutive pieces

--- lib/board.rb
+++ lib/board.rb
@@ -47,4 +47,20 @@ class Board
     end
     return nil
   end
+
+  def check_column
+    blue = "\e[34m\u2b24\e[0m"
+    red = "\e[35m\u2b24\e[0m"
+    player1 = player2 = 0
+    7.times do |column|
+      (1..6).each do |row|
+        player1 += 1 if board[row][column] == blue
+        player2 += 1 if board[row][column] == red
+        return blue if player1 == 4
+        return red if player2 == 4
+      end
+      player1 = player2 = 0
+    end
+    return nil
+  end
 end
